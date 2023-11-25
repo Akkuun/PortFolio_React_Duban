80bd0334eafbb82eb97235e431e31d54788543a6
Mathis Duban <mathis.duban@etu.umonpellier.fr>
2023-11-25 19:26:45 +0100

    add cursor custom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import React, { useEffect } from 'react';
+import './style/App.css';
 import PageMain from "./page/pageMain";
 
-import './style/App.css';
 function App() {
-  return (
-    <div className="App">
-   <PageMain/>
-    </div>
-  );
+    useEffect(() => {
+        const cursorDot = document.querySelector("[data-cursor-dot]");
+        const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+        const handleMouseMove = (e) => {
+            const posX = e.clientX;
+            const posY = e.clientY;
+
+            if (cursorDot) {
+                // Décalage du mouvement avec un léger délai de 50 millisecondes
+                setTimeout(() => {
+                    cursorDot.style.left = `${posX}px`;
+                    cursorDot.style.top = `${posY}px`;
+                }, 50);
+            }
+
+            if (cursorOutline) {
+                // Décalage du mouvement avec un léger délai de 50 millisecondes
+                setTimeout(() => {
+                    cursorOutline.style.left = `${posX}px`;
+                    cursorOutline.style.top = `${posY}px`;
+                }, 50);
+            }
+        };
+
+        window.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+        };
+    }, []); // Empty dependency array ensures this runs only once after initial render
+
+    return (
+        <div className="App">
+            <body>
+            <div className="cursor-dot" data-cursor-dot></div>
+            <div className="cursor-outline" data-cursor-outline></div>
+            </body>
+            <PageMain />
+        </div>
+    );
 }
 
 export default App;
