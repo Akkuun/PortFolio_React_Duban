2ca8f36cdabc8d990b76510a81bea6dd9f1a665b
Mathis Duban <mathis.duban@etu.umonpellier.fr>
2023-11-26 14:21:24 +0100

    pic added

--- src/component/HeaderComponent.js
+++ src/component/HeaderComponent.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import "..//style//header-component-style.css";
+import "../style/css/header-component-style.css";
 import {TypeAnimation} from "react-type-animation";
-
+import pic from "../style/picture/picture_about_me.PNG"
 
 const HeaderComponent = () => {
 
     return (
 
         <div className="header-component">
+
             <div className="Name">
                 <h4 id="Hi"> Hi , my name is </h4>
                 <h1 id="Mathis"> Mathis Duban</h1>
@@ -26,8 +27,13 @@ const HeaderComponent = () => {
                         style={{fontSize: '1em', display: 'inline-block'}}
                         repeat={Infinity}
                     />
-
                 </h1>
+
+                <div id="picture_about_me">
+                    <img src={pic} alt="pic" id="pic" height="80%" width="80%"/>
+                </div>
+
+
 
             </div>
 
