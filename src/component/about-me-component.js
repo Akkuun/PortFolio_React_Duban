a21a43478df6f9d0914c08a92658967c60d96303
Mathis <90316879+Akkuun@users.noreply.github.com>
2024-02-02 11:24:21 +0100

    ajout du component about me avec animation

--- src/component/about-me-component.js
+++ src/component/about-me-component.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import "../style/css/aboutMeComponent.css";
-const aboutMeComponent =() => {
+import {TypeAnimation} from "react-type-animation";
+
+const aboutMeComponent = () => {
     return (
-        <div id="aboutmecomponent">
-            adkjkjvhjvhs
+        <div id="text_me">
+            <div id="aboutmecomponent">
+                <TypeAnimation
+                    sequence={[
+                        'I am a passionate developer who loves to learn new technologies and use DevOps tools and methods tototo',
+                        300,
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    style={{fontSize: '1em', display: 'inline-block', paddingTop: '1.5em'}}
+                    repeat={1}
+                />
+
+
+
+            </div>
         </div>
     )
 }
