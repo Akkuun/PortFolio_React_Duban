fc928a02941666ee7be18b8973646a9ce3365d83
unknown <mathis.duban@etu.umontpellier.fr>
2024-02-17 22:06:32 +0100

    cursor modified

--- src/index.js
+++ src/index.js
@@ -7,43 +7,11 @@ import AnimatedCursor from "react-animated-cursor";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const Component = () => {
-    const buttonRef = useRef(null);
-
-    useEffect(() => {
-        if (buttonRef.current) {
-            buttonRef.current.click();
-
-        }
-    }, []);
 
     return (
         <div className="App">
-            <AnimatedCursor
-                innerSize={20}
-                outerSize={50}
-                color='255, 255, 255'
-                outerAlpha={0.1}
-                innerScale={0.7}
-                outerScale={3}
-
-                clickables={[
-                    'a',
-                    'input[type="text"]',
-                    'input[type="email"]',
-                    'input[type="number"]',
-                    'input[type="submit"]',
-                    'input[type="image"]',
-                    'label[for]',
-                    'select',
-                    'textarea',
-                    'button',
-                    '.link',
-                    '.box'
-                ]}
-            />
-
+            <AnimatedCursor color='255,255,255' outerScale={3} innerSize={20} outerSize={50}  />
             <App />
-            <button ref={buttonRef} style={{ display: 'none' }} onClick={() => console.log('Click event triggered!')} />
         </div>
     );
 };
