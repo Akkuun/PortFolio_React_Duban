0cf8fdf3ead3473b113f7d09114066f8b99037b6
Léa <leaserrano1119@gmail.com>
2024-02-20 13:03:23 +0100

    Basic functionality of the timeline

--- src/component/TimelineComponent.js
+++ src/component/TimelineComponent.js
@@ -1,10 +1,45 @@
 import "../style/css/timeline-component.css";
+import { Stepper, Step, StepLabel, StepConnector } from '@mui/material';
+import * as React from 'react';
 
 const TimelineComponent = () => {
 
     return (
         <div className="timeline-component">
-            <h1 className="PageTitle"> Studies and experiences</h1>
+            <div id="timelineTitle">
+                <h1 className="PageTitle"> Studies and experiences</h1>
+            </div>
+
+            <div id="timelineBody">
+            <div className="horizontal-scroll">
+                <Stepper className="horizontal-stepper" alternativeLabel connector={<StepConnector />}>
+                    <Step>
+                        <StepLabel id="test1">Bac STI2D</StepLabel>
+                    </Step>
+                    <Step>
+                        <StepLabel className="stepStyle">Step 2</StepLabel>
+                    </Step>
+                    <Step>
+                        <StepLabel className="stepStyle">Step 3</StepLabel>
+                    </Step>
+                    <Step>
+                        <StepLabel>Step 1</StepLabel>
+                    </Step>
+                    <Step>
+                        <StepLabel>Step 2</StepLabel>
+                    </Step>
+                    <Step>
+                        <StepLabel>Step 3</StepLabel>
+                    </Step>
+                    <Step>
+                        <StepLabel>Step 1</StepLabel>
+                    </Step>
+                    <Step>
+                        <StepLabel>Step 2</StepLabel>
+                    </Step>
+                </Stepper>
+                </div>
+            </div>
         </div>
         );
     };
