b4655226833ae694c7edf6573d492a04f9ec07f8
Léa <leaserrano1119@gmail.com>
2024-02-25 20:58:26 +0100

    End of the timeline base

--- src/component/TimelineComponent.js
+++ src/component/TimelineComponent.js
@@ -1,51 +1,40 @@
 import "../style/css/timeline-component.css";
-import { Stepper, Step, StepLabel, StepConnector } from '@mui/material';
+import { Stepper, Step, StepLabel, StepConnector, Typography } from '@mui/material';
 import * as React from 'react';
+import github from "../style/picture/school.png";
 
 const TimelineComponent = () => {
+    const steps = [
+        { title: 'Step 1', date: '01/01/2022', description: 'Description de l\'étape 1', position: "stepLabelBottom"},
+        { title: 'Step 2', date: '02/01/2022', description: 'Description de l\'étape 2', position: "stepLabelTop"},
+        { title: 'Step 3', date: '03/01/2022', description: 'Description de l\'étape 3', position: "stepLabelTop"},
+        { title: 'Step 4', date: '04/01/2022', description: 'Description de l\'étape 4', position: "stepLabelBottom"}
+      ];
 
     return (
         <div className="timeline-component">
-            <div id="timelineTitle">
-                <h1 className="PageTitle"> Studies and experiences</h1>
-            </div>
-
             
-
-            <div className="horizontal-scroll">
-                <Stepper className="horizontal-stepper" alternativeLabel connector={<StepConnector />}>
-                    <Step>
-                        <StepLabel /*className="stepLabel"*/>
-                            <h4 className = "stepTitle">Bac STI2D</h4>
-                            <h5 className = "stepDate">2019</h5>
-                            <h5 className = "stepText">J'ai réalisé un bac STI2D balblabalbalablablaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaouuuuuuuuuuuuuuuuuuuuu</h5>
-                        </StepLabel>
-                    </Step>
-                    <Step>
-                        <StepLabel className="stepStyle">Step 2</StepLabel>
-                    </Step>
-                    <Step>
-                        <StepLabel className="stepStyle">Step 3</StepLabel>
-                    </Step>
-                    <Step>
-                        <StepLabel>Step 1</StepLabel>
-                    </Step>
-                    <Step>
-                        <StepLabel>Step 2</StepLabel>
-                    </Step>
-                    <Step>
-                        <StepLabel>Step 3</StepLabel>
-                    </Step>
-                    <Step>
-                        <StepLabel>Step 1</StepLabel>
-                    </Step>
-                    <Step>
-                        <StepLabel>Step 2</StepLabel>
-                    </Step>
-                </Stepper>
+                <div id="timelineTitle">
+                    <h1 className="PageTitle"> Studies and experiences</h1>
                 </div>
+
+                <div className="horizontal-scroll">
+                    <Stepper className="horizontal-stepper" alternativeLabel activeStep={0} connector={<StepConnector />}>
+                        {steps.map((step, index) => (
+                            <Step key={index}>
+                                <StepLabel>
+                                    <div className={step.position}>
+                                    <h4 className="stepTitle">{step.title}</h4>
+                                    <h4 className="stepDate">{step.date}</h4>
+                                    <h4 className="stepDescription">{step.description}</h4>
+                                    </div>
+                                </StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+            </div>
         </div>
-        );
-    };
-    
-    export default TimelineComponent;
+    );
+};
+
+export default TimelineComponent;
