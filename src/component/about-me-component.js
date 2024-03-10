db936bbfbd0be956b426e387b6c4eeb67beaac44
unknown <mathis.duban@etu.umontpellier.fr>
2024-03-10 23:05:27 +0100

    text modify

--- src/component/about-me-component.js
+++ src/component/about-me-component.js
@@ -8,13 +8,14 @@ const aboutMeComponent = () => {
             <div id="aboutmecomponent">
                 <TypeAnimation
                     sequence={[
-                        'I am a passionate developer who loves to learn new technologies and use DevOps tools and methods',
+                        'I am a passionate developer who loves learning new technologies, working on various projects and crossing fields of application.',
                         300,
+
                     ]}
                     wrapper="span"
                     speed={50}
                     style={{fontSize: '1em', display: 'inline-block', paddingTop: '1.5em'}}
-                    repeat={1}
+                    repeat={Infinity}
                 />
 
 
