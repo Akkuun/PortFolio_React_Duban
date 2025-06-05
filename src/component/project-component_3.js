a915c761a6cd15b484e685ddc8489f4056c4c691
Akkuun <mathisduban@gmail.com>
2025-06-05 16:54:31 +0200

    new projects added

--- src/component/project-component_3.js
+++ src/component/project-component_3.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import '../style/css/project-component.css';
 import ProjectItem from "./project-item";
-import Friendly from '../style/picture/Friendly.png';
-import CGJ2022 from '../style/picture/CGJ20222.png';
-import site2 from '../style/picture/site_2.png';
+import Minecraft from '../style/picture/minecraft.png';
+import SLIC from '../style/picture/slic.jpg';
+import ecoPLant from '../style/picture/ecoPlant.png';
+import raytracing from '../style/picture/raytracing.png';
 const ProjectComponent_3 = () => {
     const projects = [
         {
-            title: 'Summer Game Jam 2023 C#',
-            image: Friendly,
+            title: 'Raytracing C++ & OpenGL',
+            image: raytracing,
             gridColumn: '1 / span 2', // Occupe les 2 premières colonnes en haut
             gridRow: '1 / span 1', // Uniquement en haut
             colorTitle: 'white',
-            url: 'https://github.com/Akkuun/Code_Game_Jam_Ete_2022'
+            url: 'https://github.com/Akkuun/M1_RayTracing_Duban'
         },
         {
-            title: 'Code Game Jam 2022 C#',
-            image: CGJ2022,
+            title: 'Adavanced Minecraft clone C++ & OpenGL',
+            image: Minecraft,
             gridColumn: '2 / span 1',
             gridRow: '1 / span 2', // Uniquement en bas
             colorTitle: 'white',
-            url: 'https://github.com/Akkuun/CodeGameJam2022'
+            url: 'https://github.com/Akkuun/MinerFabriquer'
         },
         {
-            title: 'Personal portfolio React',
-            image: site2,
+            title: 'Superpixels image processing C++',
+            image: SLIC,
             gridColumn: '2 / span 1', // Uniquement la première colonne
             gridRow: '1 / span 1', // Uniquement en bas
-            colorTitle: 'white',
-            url: 'https://github.com/Akkuun/PortFolio_React_Duban'
+            colorTitle: 'black',
+            url: 'https://github.com/Akkuun/Super-Pixel-Project'
         }
     ];
 
